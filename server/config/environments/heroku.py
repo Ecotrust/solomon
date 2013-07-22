cbd5780fdfb9f5969a40555247b97797b86c41fa
Edwin Knuth <eknuth@ecotrust.org>
2013-07-22 14:08:44 -0700

    wildcard allowed hots

--- server/config/environments/heroku.py
+++ server/config/environments/heroku.py
@@ -11,8 +11,6 @@ DATABASES['default'] =  dj_database_url.config()
 DEBUG = False
 HEROKU = True
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Allow all host headers
 ALLOWED_HOSTS = ['*']
