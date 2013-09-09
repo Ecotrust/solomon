35bba7a960cad48ae744c2fcd5c65e7780afcf4a
Edwin Knuth <eknuth@ecotrust.org>
2013-09-09 14:00:17 -0700

    grid val bug with dash, apk

--- server/static/survey/assets/js/controllers/SurveyDetail.js
+++ server/static/survey/assets/js/controllers/SurveyDetail.js
@@ -423,7 +423,7 @@ angular.module('askApp')
             // validate grid questions
             _.each($scope.question.grid_cols, function (col) {
                 _.each(answer, function (gridAnswer) {
-                    if (col.required && (gridAnswer[col.label] === undefined || gridAnswer[col.label] === null)) {
+                    if (col.required && (gridAnswer[col.label.replace('-', '')] === undefined || gridAnswer[col.label.replace('-', '')] === null)) {
                         gridValidated = false;
                     }    
                 });
