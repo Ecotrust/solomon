f3532182443eaa69d0dbfaeb70c4f059ca9f2f62
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-11-13 16:02:49 -0800

    Barcharts switched over to highcharts. Not quite how I want them.

--- server/static/survey/assets/js/directives/charts.js
+++ server/static/survey/assets/js/directives/charts.js
@@ -11,58 +11,44 @@ angular.module('askApp')
 
             link: function postLink(scope, element, attrs) {
 
-
-
                 scope.$watch('chart', function(newValue) {
                     // Draw the graph
                     var labels, data;
 
                     if (newValue && !newValue.message) {
-                        labels = _.map(scope.chart.labels, function(item, index) {
-                            return [index, item];
-                        });
                         data = _.map(scope.chart.data, function(item, index) {
-                            return [index, item];
+                            return parseFloat(item);
                         });
 
-                        Flotr.draw(
-                            element[0], [data], {
-                                HtmlText: false,
-                                fontSize: 12,
-                                mouse: {
-                                    track: false
-                                },
-                                bars: {
-                                    show: true,
-                                    horizontal: false,
-                                    shadowSize: 0,
-                                    barWidth: 0.5
-                                },
-                                mouse: {
-                                    track: true,
-                                    relative: true
-                                },
-                                xaxis: {
-                                    fontSize: 12,
-                                    ticks: labels,
-                                    title: scope.chart.xLabel
-                                },
-                                yaxis: {
-                                    fontSize: 12,
-                                    min: 0,
-                                    autoscaleMargin: 1,
-                                    autscale: true,
-                                    title: scope.chart.yLabel,
-                                    titleAngle: 90
-
+                        $(element[0]).highcharts({
+                            chart: {
+                                type: 'column'
+                            },
+                            backgroundColor: 'rgba(255, 255, 255, 0)',
+                            title: {
+                                text: false
+                            },
+                            xAxis: {
+                                categories: scope.chart.labels,
+                                title: {
+                                    text: scope.chart.xLabel
                                 }
-                            }
-                        );
+                            },
+                            yAxis: {
+                                title: {
+                                    text: scope.chart.yLabel
+                                }
+                            },
+                            series: [{
+                                name: "test",
+                                data: data,
+                                dataLabels: {
+                                    enabled: true,
+                                }
+                            }]
+                        });
                     }
-
-
                 })
-
             }
         }
     });
