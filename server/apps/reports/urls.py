169c3e8209af8e6c93bdc0426cf05ff845073b99
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-12-03 15:38:45 -0800

    test speed up and surveyor_stats view

--- server/apps/reports/urls.py
+++ server/apps/reports/urls.py
@@ -1,9 +1,10 @@
-from django.conf.urls import patterns
-from reports.views import get_crosstab, get_distribution, get_geojson
+from django.conf.urls import patterns, url
+from reports.views import get_crosstab, get_distribution, get_geojson, surveyor_stats
 
 
 urlpatterns = patterns('',
     (r'/distribution/(?P<survey_slug>[\w\d-]+)/(?P<question_slug>[\w\d-]+)', get_distribution),
     (r'/crosstab/(?P<survey_slug>[\w\d-]+)/(?P<question_a_slug>[\w\d-]+)/(?P<question_b_slug>[\w\d-]+)', get_crosstab),
     (r'/geojson/(?P<survey_slug>[\w\d-]+)/(?P<question_slug>[\w\d-]+)', get_geojson),
+    url(r'/surveyor-stats/(?P<survey_slug>[\w\d-]+)/(?P<interval>[\w]+)', surveyor_stats, name='reports_surveyor_stats'),
 )
