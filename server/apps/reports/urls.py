43e5441fee2ff424d5827f72a6a827b6413a8432
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-12-10 16:39:39 -0800

    remove .json from the json urls
    json will be the implicit response with csv as an explicit alternative

--- server/apps/reports/urls.py
+++ server/apps/reports/urls.py
@@ -6,10 +6,10 @@ from reports.views import (get_crosstab_json, get_crosstab_csv,
 
 urlpatterns = patterns('',
     (r'/distribution/(?P<survey_slug>[\w\d-]+)/(?P<question_slug>[\w\d-]+)', get_distribution),
-    (r'/crosstab/(?P<survey_slug>[\w\d-]+)/(?P<question_a_slug>[\w\d-]+)/(?P<question_b_slug>[\w\d-]+).json', get_crosstab_json),
+    (r'/crosstab/(?P<survey_slug>[\w\d-]+)/(?P<question_a_slug>[\w\d-]+)/(?P<question_b_slug>[\w\d-]+)', get_crosstab_json),
     (r'/crosstab/(?P<survey_slug>[\w\d-]+)/(?P<question_a_slug>[\w\d-]+)/(?P<question_b_slug>[\w\d-]+).csv', get_crosstab_csv),
     (r'/geojson/(?P<survey_slug>[\w\d-]+)/(?P<question_slug>[\w\d-]+)', get_geojson),
-    url(r'/surveyor-stats/(?P<survey_slug>[\w\d-]+)/(?P<interval>[\w]+).json', surveyor_stats_json, name='reports_surveyor_stats_json'),
+    url(r'/surveyor-stats/(?P<survey_slug>[\w\d-]+)/(?P<interval>[\w]+)', surveyor_stats_json, name='reports_surveyor_stats_json'),
     url(r'/surveyor-stats/(?P<survey_slug>[\w\d-]+)/(?P<interval>[\w]+).csv', surveyor_stats_csv, name='reports_surveyor_stats_csv'),
     url(r'/surveyor-stats/(?P<survey_slug>[\w\d-]+).csv', surveyor_stats_raw_data_csv, name='reports_surveyor_stats_raw_data_csv'),
 )
