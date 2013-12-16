564e6339a7d099a58ea5ac349317cd028dbf3d68
Wraithan (Chris McDonald) <xwraithanx@gmail.com>
2013-12-16 00:06:44 -0800

    add csrf token to patch

--- server/static/survey/assets/js/controllers/RespondantDetail.js
+++ server/static/survey/assets/js/controllers/RespondantDetail.js
@@ -1,8 +1,26 @@
 
+// using jQuery
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie != '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+
 angular.module('askApp')
     .config(['$httpProvider', function($httpProvider) {
         $httpProvider.defaults.headers.patch = {
-            'Content-Type': 'application/json;charset=utf-8'
+            'Content-Type': 'application/json;charset=utf-8',
+            'X-CSRFToken': getCookie('csrftoken')
         }
     }])
     .controller('RespondantDetailCtrl', function($scope, $routeParams, $http, $location) {
