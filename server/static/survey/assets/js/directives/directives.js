40f6bea89ece9eae83439a3979be0fda2bb00c60
Quinlan Pfiffer <qpfiffer@gmail.com>
2013-12-16 14:55:45 -0800

    Added print buttons to both the survey stats and overview pages. Made the print button a directive because you can't use window in ng-click.

--- server/static/survey/assets/js/directives/directives.js
+++ server/static/survey/assets/js/directives/directives.js
@@ -1,5 +1,18 @@
 
 angular.module('askApp')
+    .directive('printButton', function() {
+        return {
+            template: '<button id="print_button" class="btn btn-success" ng-click="print()"><i class="icon-print"></i> Print</button>',
+            restrict: 'EA',
+            transclude: true,
+            replace: true,
+            link: function(scope) {
+                scope.print = function() { window.print(); };
+            }
+        }
+    }
+);
+angular.module('askApp')
     .directive('datePicker', function() {
 
     return {
