08824fa8041103a666a43221771511b7e36874db
Edwin Knuth <edwin@pointnineseven.com>
2013-12-16 16:45:13 -0800

    fixing api keys

--- server/apps/account/migrations/0002_api_keys.py
+++ server/apps/account/migrations/0002_api_keys.py
@@ -1,19 +1,16 @@
 # -*- coding: utf-8 -*-
-import datetime
-from south.db import db
 from south.v2 import DataMigration
-from django.db import models
 from tastypie.models import ApiKey
 from django.contrib.auth.models import User
+
 
 class Migration(DataMigration):
 
     def forwards(self, orm):
         "Write your forwards methods here."
-        # Note: Don't use "from appname.models import ModelName". 
+        # Note: Don't use "from appname.models import ModelName".
         # Use orm.ModelName to refer to models in this application,
         # and orm['appname.ModelName'] for models in other applications.
-        #for u in orm['auth.user'].objects.all():
         #doesn't like the frozen orm model :()
         for u in User.objects.all():
             ApiKey.objects.get_or_create(user=u)
